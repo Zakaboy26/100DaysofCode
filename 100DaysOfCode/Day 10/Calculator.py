640e1f729f5ba59ffe95db162d89b16233b589ad
Zakaboy26 <zakariyaabdirizak@outlook.com>
2025-11-12 11:06:30 +0000

    Day 10

---
+++ 100DaysOfCode/Day 10/Calculator.py
@@ -0,0 +1,55 @@
+import art
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+calculator_dictionary = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+def calculator():
+    first_number = float(input("What's the first number?: "))
+    should_continue = True
+
+    while should_continue:
+        operation_pick = input("+\n-\n*\n/\nPick an operation: ")
+        next_number = float(input("What's the next number?: "))
+        answer = calculator_dictionary[operation_pick](first_number, next_number)
+        print(f"{first_number} {operation_pick} {next_number} = {answer}")
+
+        continue_answer = input(f"Type 'Y' to continue calculating with {answer} or type 'N' to start a new calculator").upper()
+        if continue_answer == "Y":
+            first_number = answer
+        elif continue_answer == "N":
+            should_continue = False
+            calculator()
+        else:
+            print("Invalid input")
+            should_continue = False
+
+print(art.logo)
+calculator()
+
+
+
+
+
+
+
+
+
+
+
+
